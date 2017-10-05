d056cc66566b095485bb69699a8fa103acdc5a65
Oleksandr Borysenko <borisenko.alexandr@gmail.com>
2017-10-05 13:59:19 +0300

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -4,3 +4,4 @@ docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 
 sudo rm -rf ../crypto-config ../configtx.yaml ../orderer.block ../channel.tx
+cd ~/azure_fabric/ && git pull && cp ./* ../ && cd ~
