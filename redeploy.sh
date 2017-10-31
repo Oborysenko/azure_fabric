9bf87dca8cac2093059c494a9aa20d7a991bc91e
Oleksandr Borysenko <borisenko.alexandr@gmail.com>
2017-10-31 11:11:45 +0200

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -3,5 +3,7 @@
 sudo docker kill $(sudo docker ps -q)
 sudo docker rm $(sudo docker ps -a -q)
 
+git stash
 sudo rm -rf ./crypto-config ./configtx.yaml ./orderer.block ./channel.tx ./crypto-config.yaml
 cd ~/fabric/ && git pull && cp -r ./* ../ && cd ~/
+chmod +x ~/fabric/redeploy.sh
