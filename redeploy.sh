954aed08d00ba94e1dcbcea877424001fa4af678
Oleksandr Borysenko <borisenko.alexandr@gmail.com>
2017-10-31 15:05:34 +0200

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -5,7 +5,7 @@ sudo docker rm $(sudo docker ps -a -q)
 
 git stash
 sudo rm -rf ./crypto-config ./configtx.yaml ./orderer.block ./channel.tx ./crypto-config.yaml
-cd ~/fabric/ && git pull && cp -r ./* ../ && cd ~/
+cd ~/fabric/ && git pull
 chmod +x ~/fabric/redeploy.sh
 chmod +x ~/fabric/configure-fabric-azureuser.sh
 chmod +x ~/fabric/yeasy_conf.sh
